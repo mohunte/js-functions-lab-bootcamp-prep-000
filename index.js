5970ccd4d505909da69013605898a0016bb5cafe
Monique Hunte <monhunte@gmail.com>
2018-09-27 16:27:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // write your code below!
+
+describe ('functions'){
+  describe ('happyHolidays'){
+    it('returns "Happy Holidays!"'){
+      expect (happyHolidays()).toEqual('Happy Holidays!')
+    }
+  }
+}
+
+describe ('happyHolidaysToMonique'){
+  it('returns "Happy Holidays, Monique"'){
+    expect (happyHolidaysToMonique).toEqual('Happy Holidays, Monique!')
+  }
+}
+
+describe ('happyHolidayTo(Birthday, Monique'){
+  it('returns "Happy Birthday, Monique!"'){
+    expect (happyHoliday)
+  }
+}
